9319723dfb0ee5e311f71df793e32c5bd678336f
Goel-Ben_Eliezer <goelbe@gmail.com>
2021-03-28 21:56:49 +0300

    three

---
+++ text.py
@@ -0,0 +1,41 @@
+a = open("text.txt")
+a = a.read()
+a=a.splitlines()
+word = a[0]
+a[0]=" "
+
+
+def revword(w):
+    c=''.join(reversed(w))
+    c=c.lower()
+    #mm=c.split(' ')
+    return c
+
+
+def countword(a):
+    i=0
+    count=1
+    while i < len(a):
+        j=0
+        s=revword(a[i]).split(' ')
+        while j<len(s): 
+            if word in s[j]:
+                count=count+1
+            j=j+1
+        i=i+1
+    return count
+                
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
